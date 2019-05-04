8f4f7e0ad5695bfa1a30d2415c0221b497b239ce
Nazaret <nazaretrogue@correo.ugr.es>
2019-05-05 01:02:17 +0200

    Added color space and color bands processing.

---
+++ P11/README.md
@@ -0,0 +1,7 @@
+# Práctica 11: Procesamiento de imágenes (parte 3)
+
+Se utilizan los distintos espacios de color para dar lugar a un herramienta más
+potente respecto al procesamiento:
+
++ Se cambia el espacio de color entre tres posibilidades disponibles: RGB, YCC, escala de grises.
++ Se extraen las distintas bandas de los distintos espacios de color.
---
+++ P11/SM.NRG.Biblioteca/src/sm/nrg/iu/ColorRender.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sm.nrg.iu;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+/**
+ *
+ * @author nazaret
+ */
+public class ColorRender extends PanelColor implements ListCellRenderer<Color>{
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Color> list, Color value, int index, boolean isSelected, boolean cellHasFocus) {
+        PanelColor p = new PanelColor(value);
+        p.setSize(80, 30);
+        
+        return p;
+    }
+    
+    
+    
+}
